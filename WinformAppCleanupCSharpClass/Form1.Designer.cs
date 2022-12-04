aad3987eb999017c01cc91ee245bbc8626aaa7a8
cedd82 <cwarnant@gmail.com>
2022-12-04 16:58:01 +1100

    x

--- WinformAppCleanupCSharpClass/Form1.Designer.cs
+++ WinformAppCleanupCSharpClass/Form1.Designer.cs
@@ -31,13 +31,16 @@ partial class Form1
             this.btnProcess = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.btnProcessFolder = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnProcess
             // 
-            this.btnProcess.Location = new System.Drawing.Point(12, 641);
+            this.btnProcess.Location = new System.Drawing.Point(14, 855);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnProcess.Name = "btnProcess";
-            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.Size = new System.Drawing.Size(86, 31);
             this.btnProcess.TabIndex = 0;
             this.btnProcess.Text = "button1";
             this.btnProcess.UseVisualStyleBackColor = true;
@@ -45,28 +48,51 @@ partial class Form1
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(12, 27);
+            this.txtInput.Location = new System.Drawing.Point(14, 36);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(552, 235);
+            this.txtInput.Size = new System.Drawing.Size(630, 312);
             this.txtInput.TabIndex = 1;
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(12, 342);
+            this.txtOutput.Location = new System.Drawing.Point(14, 456);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
-            this.txtOutput.Size = new System.Drawing.Size(552, 235);
+            this.txtOutput.Size = new System.Drawing.Size(630, 312);
             this.txtOutput.TabIndex = 2;
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Location = new System.Drawing.Point(825, 402);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(125, 27);
+            this.txtInputFolder.TabIndex = 3;
+            // 
+            // btnProcessFolder
+            // 
+            this.btnProcessFolder.Location = new System.Drawing.Point(825, 454);
+            this.btnProcessFolder.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnProcessFolder.Name = "btnProcessFolder";
+            this.btnProcessFolder.Size = new System.Drawing.Size(171, 31);
+            this.btnProcessFolder.TabIndex = 4;
+            this.btnProcessFolder.Text = "processFolder";
+            this.btnProcessFolder.UseVisualStyleBackColor = true;
+            this.btnProcessFolder.Click += new System.EventHandler(this.btnProcessFolder_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(938, 676);
+            this.ClientSize = new System.Drawing.Size(1313, 901);
+            this.Controls.Add(this.btnProcessFolder);
+            this.Controls.Add(this.txtInputFolder);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.btnProcess);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -79,4 +105,6 @@ partial class Form1
     private Button btnProcess;
     private TextBox txtInput;
     private TextBox txtOutput;
+    private TextBox txtInputFolder;
+    private Button btnProcessFolder;
 }
--- WinformAppCleanupCSharpClass/Form1.cs
+++ WinformAppCleanupCSharpClass/Form1.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace WinformAppCleanupCSharpClass;
@@ -30,7 +32,103 @@ public partial class Form1 : Form
     //    Regex r = new Regex("public partial class CreateSearchRequest) ")
     //}
 
-    private string StripComments(string input)
+    private static string StripComments(string input)
+    {
+        var blockComments = @"/\*(.*?)\*/";
+        var lineComments = @"//(.*?)\r?\n";
+        var strings = @"""((\\[^\n]|[^""\n])*)""";
+        var verbatimStrings = @"@(""[^""]*"")+";
+        var noComments = Regex.Replace(input,
+            blockComments + "|" + lineComments + "|" + strings + "|" + verbatimStrings,
+            me =>
+            {
+                if (me.Value.StartsWith("/*") || me.Value.StartsWith("//"))
+                    return me.Value.StartsWith("//") ? Environment.NewLine : "";
+                // Keep the literal strings
+                return me.Value;
+            },
+            RegexOptions.Singleline);
+        return noComments;
+    }
+
+    private void btnProcessFolder_Click(object sender, EventArgs e)
+    {
+        var folder = txtInputFolder.Text;
+        folder = @"C:\code\cyberSourceAll\cybersource-rest-client-dotnetstandard\cybersource-rest-client-netstandard\cybersource-rest-client-netstandard\Model";
+        var di = new DirectoryInfo(folder);
+        if (!di.Exists)
+            return;
+
+        var ticks = DateTime.Now.Ticks;
+        var outDirStr = $@"c:\temp\cybersource-rest-client-netstandard\Model{ticks}";
+        DirectoryInfo outFolder = Directory.CreateDirectory(outDirStr);
+
+        FileInfo[] files = di.GetFiles("*.cs");
+        
+        FileInfo firstF = files[0];
+        //ProcessInputFile.ProcessFile(firstF, outFolder);
+        foreach (var fileInfo in files)
+        {
+            ProcessInputFile.ProcessFile(fileInfo, outFolder);  
+        }
+
+        var projFileContent = 
+@"<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net7.0</TargetFramework>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <Nullable>enable</Nullable>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include=""BouncyCastle.NetCore"" Version=""1.9.0"" />
+    <PackageReference Include=""jose-jwt"" Version=""4.0.1"" />
+    <PackageReference Include=""Newtonsoft.Json"" Version=""13.0.1"" />
+  </ItemGroup>
+
+
+</Project>
+";
+        var csProjFilePath = Path.Combine(outDirStr, $"{DateTime.Now.Ticks}Models.csproj");
+        File.WriteAllText(csProjFilePath, projFileContent);
+
+
+
+        Process.Start("explorer.exe", outFolder.FullName);
+    }
+}
+
+public static class ProcessInputFile
+{
+    public static void ProcessFile(FileInfo inputFile, DirectoryInfo outputDirectoryInfo)
+    {
+        var fullName = inputFile.FullName;
+        var allText = File.ReadAllText(fullName);
+        //var allText = File.ReadAllText(fullName);
+
+        allText = StripComments(allText);
+
+        var isMatch = false;
+        var removeEmptyLines = @"^(\s)*$\n";
+        var removeEmptyLinesRegex = new Regex(removeEmptyLines, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        isMatch = removeEmptyLinesRegex.IsMatch(allText);
+        allText = removeEmptyLinesRegex.Replace(allText, "");
+
+        var outputDir = outputDirectoryInfo;
+        var outFileName = inputFile.Name;
+        //var outFileNameTemp = $"{inputFile.Name}tmp.cs";
+        var outFileNameTemp = Path.Combine(outputDir.FullName, outFileName);
+
+        File.WriteAllText(outFileNameTemp, allText);
+
+        //var regexCtorString = @"^\s*public " + className + @".[\s*.*\{\w=\(\),;}]*";
+        //var removeCtorRegex = new Regex(regexCtorString, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        //isMatch = removeCtorRegex.IsMatch(allText);
+
+    }
+
+    private static string StripComments(string input)
     {
         var blockComments = @"/\*(.*?)\*/";
         var lineComments = @"//(.*?)\r?\n";
